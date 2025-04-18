d4d2f2ae73e1a404d2b2a54d5ffc243b8e55f20b
AllKarr <allen.karro@gmail.com>
2025-04-18 13:32:10 +0200

    Update budgetModel.ts

--- src/models/budgetModel.ts
+++ src/models/budgetModel.ts
@@ -4,7 +4,6 @@ const budgetSchema = new mongoose.Schema(
   {
     category: { type: String, required: true },
     limit: { type: Number, required: true },
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true }
   },
   { timestamps: true }
 );
